a610b4c812bf9ea5bf83adcd30430cf8a79fcfd6
ddalkigum <sol35352000@gmail.com>
2021-01-15 11:14:48 +0900

    ADD: 메인페이지 카드, 리뷰카드

--- enter_cloud/urls.py
+++ enter_cloud/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('user', include('users.urls'))
-    
+    path('user', include('users.urls')),
+    path("spaces", include("spaces.urls"))
 ]
--- reviews/management/commands/seed_reviews.py
+++ reviews/management/commands/seed_reviews.py
@@ -2,6 +2,7 @@ import random
 
 from django_seed                    import Seed
 from django.core.management.base    import BaseCommand
+from django.contrib.admin.utils     import timezone
 
 from reviews.models      import Review
 from users.models        import User
@@ -24,12 +25,13 @@ class Command(BaseCommand):
             Review, 
             number,
             {
-                "user":      lambda x : random.choice(users),
-                "space":     lambda x : random.choice(spaces),
-                "rating":    lambda x : random.randint(3, 5),
-                "content":   lambda x : random.choice(review_list)
+                "user":         lambda x : random.choice(users),
+                "space":        lambda x : random.choice(spaces),
+                "rating":       lambda x : random.randint(3, 5),
+                "content":      lambda x : random.choice(review_list),
+                "created_at":   lambda x : timezone.localtime(),
+                "updated_at":   lambda x : timezone.localtime()
             }
         )
         seeder.execute()
         self.stdout.write(self.style.SUCCESS(f'review created {number}'))
-        
